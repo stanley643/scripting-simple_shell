34751f25d92eddd2038f3e31cac4c5d7eb9e7081
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-11 14:19:31 -0700

    main

---
+++ shell.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * main - the shell playground
+ * Return: 0 on success
+ *
+ */
+int main(int argc, char **argv, char **envp)
+{
+pid_t pid;
+
+while (1)
+{
+	pid = fork();
+
+	if (pid < 0)
+	{
+		perror("Process executrion failed");
+		continue;
+	}
+	else if (pid == 0)
+	{
+		execve();
+		perror("./shell");
+		_exit(EXIT_FAILURE);
+	}
+	wait(NULL);
+}
+return (0);
+}
