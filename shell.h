8a402fe1459555538cc1e1d46bba98149f5b0a68
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-11 13:30:05 -0700

    header files

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include<stdbool.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+
+#define BUFSIZE 1024
+#define MAX_ARGS 11
