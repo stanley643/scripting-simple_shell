e2c4b3a7bc38c9dc34da835db195a772d6843375
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-12 13:00:05 -0700

    path config

--- stprshell.c
+++ stprshell.c
@@ -8,6 +8,34 @@
 int main(int argc, char **argv, char **envp)
 {
 pid_t pid;
+char *line = NULL;
+ssize_t nread;
+size_t len = 0;
+char *args[MAX_ARGS];
+char *executable_path, *env;
+char *path[MAX_ARGS];
+char *path_var, *exit_command;
+int i;
+
+
+(void)argc;
+(void)argv;
+exit_command = "exit";
+env = "env";
+path_var = getenv("PATH");
+
+if (path_var == NULL)
+{
+	perror("./hsh");
+	return (EXIT_FAILURE);
+}
+
+i = 0;
+path[i++] = strtok(path_var, ":");
+while ((path[i] = strtok(NULL, ":")) != NULL)
+{
+	i++;
+}
 
 while (1)
 {
