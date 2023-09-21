9ba9e64becff22df6a42e4b2176806a0751ff881
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-20 19:39:11 -0700

    sh

--- stpr-path.c
+++ stpr-path.c
@@ -1,11 +1,11 @@
-#include "stprshell.h"
+#include "stpr.h"
 /**
- * stpr_find_path - a pointer trys to access the given path using X_OK
- * @command: user input
- * @path: the array of pointers to the desired exec file
- * Return: NULL on success
- */
-char *stpr_find_path(char *command, char *path[])
+* find_the_path - an pointer that trys to access the given path using x_OK
+* @command: the given input by user
+* @path: the array of pointers to the desired executable file
+* Return: give NULL value on success
+*/
+char *find_the_path(char *command, char *path[])
 {
 	char exec_path[BUFSIZE];
 	int i;
@@ -15,19 +15,20 @@ char *stpr_find_path(char *command, char *path[])
 
 	for (i = 0; path[i] != NULL; i++)
 	{
-		snprintf(exec_path, sizeof(exec_path), "%s%s", path[i], command);
+		snprintf(exec_path, sizeof(exec_path), "%s/%s", path[i], command);
 		if (access(exec_path, X_OK) == 0)
 			return (strdup(exec_path));
+
 	}
 	return (NULL);
 }
 /**
- * stpr_initialize_path - initializes the path
+ * initialize_path - initializes the path
  * @path_var: path variable
  * @path: the path
  * Return: nothing
  */
-void stpr_initialize_path(char *path_var, char **path)
+void initialize_path(char *path_var, char **path)
 {
 	size_t i = 0;
 
