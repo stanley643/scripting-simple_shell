95de370d88e75eba18a143559f149ec791112d37
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-22 13:21:15 -0700

    shell

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define BUFSIZE 1024
+#define MAX_ARGS 11
+
+char *create_buf(char *buf);
+void _print_str(char *c);
+int _putchar(char c);
+void tokenize_args(char **args_array, char *str);
+char *find_the_path(char *command, char *path[]);
+void remove_newline(char *str);
+
+#endif
